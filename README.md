50c30914ed8389e473ee8d7460d9210498811c94
HorseMD <alightedness@gmail.com>
2014-08-13 23:07:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+#Clockwise
+
+Ruby integer class extension. Adds methods to "do something after x 'units of time'", do something for each segment of x unit of time, etc...
+It also adds basic time arithmetic, allowing very readable time calculation (e.g. `Time.now + 10.minutes`)
+
+##Usage
+
+Some usage examples:
+
+    5.minutes.each do |minute|
+        puts "The time is: #{Time.now}"
+    end
+    The time is: 2014-08-05 15:59:08 +0100
+    The time is: 2014-08-05 16:00:08 +0100
+    The time is: 2014-08-05 16:01:08 +0100
+    The time is: 2014-08-05 16:02:08 +0100
+    The time is: 2014-08-05 16:03:08 +0100
+
+    5.hours.each_second { puts "A second has passed!" }
+    ...
+    A second has passed!
+
+    2.hours.later { puts "Two hours have passed!" }
+    "Two hours have passed!"
+
+    5.minutes.and 2.seconds
+     => 302
+
+    120.seconds == 2.minutes
+     => true
+
+    Time.now + 5 hours
+     => 2014-08-05 20:55:23 +0100
+
+##Units of Time
+
+The different units of time available are as follows:
+
+* Seconds
+* Minutes
+* Hours
+* Days
+* Weeks
+* Months
+* Years
+
+##Notes
+
+This doesn't account for any time drift caused by code passed to it. This is intentional, to keep the gem as minimal as possible.
+
+If the timeunit.later method is given no block, it will return its unit of time added to the current time. For example, 5.minutes.later will return the time that it will be in 5 minutes.
+
+Each unit of time also has an `each_x` method for every unit of time smaller than itself. For example, hours has `each_minute` and `each_second`, minutes has `each_second`, and so on.
+
---
+++ lib/clockwise.rb
@@ -0,0 +1,114 @@
+class Integer
+  def seconds
+    self
+  end
+
+  def minutes
+    seconds * 60
+  end
+
+  def hours
+    minutes * 60
+  end
+
+  def days
+    hours * 24
+  end
+
+  def weeks
+    days * 7
+  end
+
+  def months
+    weeks * 4
+  end
+
+  def years
+    months * 12
+  end
+
+  def later &block
+    TimeUnit.new(self).later(&block)
+  end
+
+  def earlier
+    Time.now - self
+  end
+
+  def each &block
+    appropriate_unit = TimeUnit.get_appropriate_unit(self)
+    TimeUnit.new(self).send("each_#{appropriate_unit.to_s}", &block)
+  end
+
+  def method_missing(name, *args, &block)
+    if name.to_s.include? "each_"
+      TimeUnit.new(self).send("#{name}", &block)
+    else
+      super
+    end
+  end
+end
+
+# Class for helper methods and any time-related methods that are slightly more
+# in-depth than just basic addition.
+class TimeUnit
+  attr_reader :value
+
+  def initialize val
+    @value = val
+    raise ArgumentError, "Cannot do something for 0 time!" unless @value > 0
+  end
+
+  # Get the biggest unit of time that makes up the given number of seconds
+  def self.get_appropriate_unit val
+    if val < 1.minutes
+      return "second"
+    elsif val < 1.hours
+      return "minute"
+    elsif val < 1.days
+      return "hour"
+    elsif val < 1.weeks
+      return "day"
+    elsif val < 1.months
+      return "week"
+    elsif val < 1.years
+      return "month"
+    else
+      return "year"
+    end
+  end
+
+  # After x units of time, or, if no block is given, the time in x time units
+  def later &block
+    if block_given?
+      sleep @value
+      block.call @value
+    else
+      Time.now + @value
+    end
+  end
+
+  # For every unit of time, we create an each_[timeunit] method.
+  %w[second minute hour day week month year].each do |unit|
+    define_method("each_#{unit}") do |&arg|
+      each_x(1.send("#{unit}s"), &arg)
+    end
+  end
+
+  private
+    # each_minute each_hour each_day etc...
+    def each_x time, &block
+      if time > @value
+        raise ArgumentError, "Cannot split a unit of time into parts bigger than itself"
+      end
+      1.upto @value/time do |val|
+        sleep time
+        if block_given?
+          block.call val
+        else
+          yield val
+        end
+      end
+      nil
+    end
+end
