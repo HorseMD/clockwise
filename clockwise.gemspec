b65542c9ff26826078efc0e29753d12ad933718c
HorseMD <alightedness@gmail.com>
2014-11-16 23:16:32 +0000

    Move TimeUnit to its own file, fix a few minor details.

--- clockwise.gemspec
+++ clockwise.gemspec
@@ -2,13 +2,14 @@ Gem::Specification.new do |s|
   s.name = 'clockwise'
   s.email = 'alightedness@gmail.com'
   s.author = 'HorseMD'
-  s.version = '0.0.1'
+  s.version = '0.0.2'
   s.date = Time.now.strftime("%Y-%m-%d")
 
   s.summary = 'Adds time methods to Integer class'
   s.description = 'Integer class extension with methods to help with time manipulation.'
   s.files = [
     'lib/clockwise.rb',
+    'lib/timeunit.rb',
     'README.md',
     'clockwise.gemspec'
   ]
--- lib/clockwise.rb
+++ lib/clockwise.rb
@@ -1,3 +1,5 @@
+require './lib/time_unit.rb'
+
 class Integer
   def seconds
     self
@@ -37,7 +39,7 @@ class Integer
 
   def each &block
     appropriate_unit = TimeUnit.get_appropriate_unit(self)
-    TimeUnit.new(self).send("each_#{appropriate_unit.to_s}", &block)
+    TimeUnit.new(self).send("each_#{appropriate_unit}", &block)
   end
 
   def and time
@@ -53,66 +55,3 @@ class Integer
   end
 end
 
-# Class for helper methods and any time-related methods that are slightly more
-# in-depth than just basic addition.
-class TimeUnit
-  attr_reader :value
-
-  def initialize val
-    @value = val
-    raise ArgumentError, "Cannot do something for 0 time!" unless @value > 0
-  end
-
-  # Get the biggest unit of time that makes up the given number of seconds
-  def self.get_appropriate_unit val
-    if val < 1.minutes
-      return "second"
-    elsif val < 1.hours
-      return "minute"
-    elsif val < 1.days
-      return "hour"
-    elsif val < 1.weeks
-      return "day"
-    elsif val < 1.months
-      return "week"
-    elsif val < 1.years
-      return "month"
-    else
-      return "year"
-    end
-  end
-
-  # After x units of time, or, if no block is given, the time in x time units
-  def later &block
-    if block_given?
-      sleep @value
-      block.call @value
-    else
-      Time.now + @value
-    end
-  end
-
-  # For every unit of time, we create an each_[timeunit] method.
-  %w[second minute hour day week month year].each do |unit|
-    define_method("each_#{unit}") do |&arg|
-      each_x(1.send("#{unit}s"), &arg)
-    end
-  end
-
-  private
-    # each_minute each_hour each_day etc...
-    def each_x time, &block
-      if time > @value
-        raise ArgumentError, "Cannot split a unit of time into parts bigger than itself"
-      end
-      1.upto @value/time do |val|
-        sleep time
-        if block_given?
-          block.call val
-        else
-          yield val
-        end
-      end
-      nil
-    end
-end
